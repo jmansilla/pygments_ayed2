ce18c4276ff654a51888252949d5b9be9d9cc34d
Javier Mansilla <javimansilla@gmail.com>
2025-04-04 19:42:48 -0300

    fixed lexer to check complete words for our keywords

--- pygments_ayed2/lexer.py
+++ pygments_ayed2/lexer.py
@@ -1,5 +1,10 @@
-from pygments.lexer import RegexLexer
+from pygments.lexer import RegexLexer, words as words
 from pygments.token import Text, Comment, Operator, Keyword, Name, String, Number, Punctuation
+
+
+def _w(pipe_separed_words):
+    # shortcut for words definition
+    return words(pipe_separed_words.split('|'), suffix=r'\b')
 
 
 class Ayed2Lexer(RegexLexer):
@@ -10,19 +15,19 @@ class Ayed2Lexer(RegexLexer):
         'root': [
             (r'\s+', Text),  # whitespace
             (r'//.*?$', Comment),  # single line comment
-            (r'type|enumerate|tuple|end', Keyword.Type.Definition),  # types declarations
-            (r'int|char|bool|real|array|pointer|of', Keyword.Type),  # types
+            (_w('type|enumerate|tuple|end'), Keyword.Type.Definition),  # types declarations
+            (_w('int|char|bool|real|array|pointer|of'), Keyword.Type),  # types
             (r':=|=', Punctuation.Assignment),  # assignments (for vars, and for types)
             (r'var|:', Keyword.Declaration),  # keyword
             (r'\b[A-Z].*?\b', Keyword.Type.Enum),  # enums constants
-            (r'if|then|else|fi|while|do|od|for|to|downto', Keyword),  # control flow keywords
-            (r'alloc|free', Name.Builtin),  # built-in functions
-            (r'getenv', Name.Builtin),      # another built-in
+            (_w('if|then|else|fi|while|do|od|for|to|downto'), Keyword),  # control flow keywords
+            (_w('alloc|free'), Name.Builtin),  # built-in functions
+            (_w('getenv'), Name.Builtin),      # another built-in
             (r'->|\.', Punctuation),  # Field access
             (r'==|!=|<|<=|>|>=', Operator),  # comparison operators
             (r'\+\+|--|\+|-|\*|/|%|!', Operator),  # arithmetic operators
             (r'\&\&|\|\|', Operator),  # logical operators
-            (r'null|true|false|inf', Name.NamedLiteral),  # literals
+            (_w('null|true|false|inf'), Name.NamedLiteral),  # literals
             (r'\'.\'', String.Char),  # Char literals
             (r'\".*\"', String.String),  # Long string literals
             (r'\d+\.\d+', Number.Float),  # floating point
